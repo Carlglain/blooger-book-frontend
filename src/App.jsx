8153124096c854568bcaff21f28ccee5d6ac3e0d
Carlstorm <carlglainnyuysemo@gmail.com>
2025-11-25 05:13:05 +0100

    fix: arranged the tailwind issue then styled the post card

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,13 @@ export default function App() {
   return (
     <div>
       Welcom to Blogger Book
-      <div className="flex gap-4">
+      <div className="flex gap-4 flex-col justify-center items-center mb-4">
         {
           posts.map((post, index) => (
-            <div key={index} className="border rounded-md px-4 py-2 max-w-full">
-              <h1 style={{ color: "blue" }}>{post.title}</h1>
-              <p>{post.postText}</p>
-              <p>{post.username}</p>
+            <div key={index} className="border flex flex-col justify-between rounded-md w-100 h-100 max-h-full  max-w-full bg-white shadow-sm">
+              <h1 className="bg-blue-500 text-white text-center h-10">{post.title}</h1>
+              <p className="text-center ">{post.postText}</p>
+              <p className="bg-blue-500 text-white text-left h-10">{post.username}</p>
             </div>
 
           ))
