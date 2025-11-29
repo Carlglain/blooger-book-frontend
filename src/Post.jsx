76834ac4319ba18bb38220cef5801e9dfec67c31
Carlstorm <carlglainnyuysemo@gmail.com>
2025-11-29 05:07:28 +0100

    feat:updated the data type of the postText from string to TEXT

--- src/Post.jsx
+++ src/Post.jsx
@@ -16,9 +16,16 @@ function Post() {
   }, [id])
   if (!data) return <div>loading ....</div>
   return (
-    <div className="my-10">
-      <div>
-        {data.postText}
+    <div className="md:my-15 my-10 flex gap-4 justify-between mx-6">
+      <div className="border flex flex-col justify-between rounded-md w-170 h-150 max-h-full  max-w-full bg-white shadow-sm">
+        <h1 className="bg-blue-500 pt-6 text-white text-center h-20">{data.title}</h1>
+        <p className="text-justify text-center px-4 py-2 leading-relaxed">{data.postText}</p>
+        <p className="bg-blue-500 text-white text-left h-20 pt-6">{data.username}</p>
+
+      </div>
+      <div className="max-w-full w-170 max-h-full p-6 justify-center flex-col gap-2  flex items-center">
+        <h1>Comment Section</h1>
+        <textarea type="text-area" className="border border-gray-500 rounded-md" />
       </div>
 
     </div>);
